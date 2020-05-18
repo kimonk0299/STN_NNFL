e9f25565ae8b10a64b1d478697b209720388dbd9
VinayKanakeri <42164399+VinayKanakeri@users.noreply.github.com>
2020-05-19 00:13:03 +0530

    Update appendNshuffle.py

--- appendNshuffle.py
+++ appendNshuffle.py
@@ -5,21 +5,21 @@ import tensorflow as tf
 def append(train_data,train_label,val_data,val_label,test_data,test_label,train_aug,val_aug,test_aug):
 	m = np.size(train_data,0)
 	b = np.zeros((m, 40, 40, 1))
-    o = int((40-28)/2)
-    b[m, o:o+28, o:o+28, 1] = train_data
-    train_data = b
+	o = int((40-28)/2)
+	b[m, o:o+28, o:o+28, 1] = train_data
+	train_data = b
 	
 	m = np.size(val_data,0)
 	b = np.zeros((m, 40, 40, 1))
-    o = int((40-28)/2)
-    b[m, o:o+28, o:o+28, 1] = val_data
-    val_data = b
+	o = int((40-28)/2)
+	b[m, o:o+28, o:o+28, 1] = val_data
+	val_data = b
 	
 	m = np.size(test_data,0)
 	b = np.zeros((m, 40, 40, 1))
-    o = int((40-28)/2)
-    b[m, o:o+28, o:o+28, 1] = test_data
-    test_data = b
+	o = int((40-28)/2)
+	b[m, o:o+28, o:o+28, 1] = test_data
+	test_data = b
 	
 	train_app = np.append(train_data,train_aug,axis=0)
 	val_app = np.append(val_data,val_aug,axis=0)
