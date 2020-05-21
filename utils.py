2734e1f7648ff4e88c2749af05120fb8eaaf6c3d
VinayKanakeri <42164399+VinayKanakeri@users.noreply.github.com>
2020-05-21 11:30:49 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -42,3 +42,23 @@ def STN(input_shape=(40, 40, 1), sampling_size=(40, 40), num_classes=10):
     x = Dense(num_classes)(x)
     x = Activation('softmax')(x)
     return Model(inputs=image, outputs=x)
+
+def random_mini_batches(X, Y, mini_batch_size = 64):
+    m = X.shape[1]
+    mini_batches = []
+    num_complete_minibatches = math.floor(m/mini_batch_size)
+    
+    for k in range(0, num_complete_minibatches):
+        mini_batch_X = X[k * mini_batch_size : k * mini_batch_size + mini_batch_size, :, :]
+        mini_batch_Y = Y[k * mini_batch_size : k * mini_batch_size + mini_batch_size, :, :]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+       
+    if m % mini_batch_size != 0:
+        mini_batch_X = X[num_complete_minibatches * mini_batch_size : m, :, :]
+        mini_batch_Y = Y[num_complete_minibatches * mini_batch_size : m, :, :]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    return mini_batches
+        
+    
