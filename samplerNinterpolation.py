44292071d8fec494ddc5be98dac1790baa13bb56
VinayKanakeri <42164399+VinayKanakeri@users.noreply.github.com>
2020-05-23 23:17:34 +0530

    Update samplerNinterpolation.py

--- samplerNinterpolation.py
+++ samplerNinterpolation.py
@@ -30,10 +30,10 @@ class sample_interpolate(tf.keras.layers.Layer):
 			out_H = out_size[0]
 			out_W = out_size[1]
 
-			batch_grids = grid_gen(out_H,out_W,theta_mat)
+			batch_grids = self.grid_gen(out_H,out_W,theta_mat)
 			
 		else:
-			batch_grids = grid_gen(Height, Width, theta_mat)
+			batch_grids = self.grid_gen(Height, Width, theta_mat)
 
 		x_s = batch_grids[:, 0, :, :]
 		y_s = batch_grids[:, 1, :, :]
