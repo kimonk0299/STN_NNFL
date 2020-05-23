de9c1cabd7e0171c5db72869ad6b4a7991649b8f
VinayKanakeri <42164399+VinayKanakeri@users.noreply.github.com>
2020-05-23 22:21:38 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import matplotlib.pyplot as plt
 import math
 
 from samplerNinterpolation import sample_interpolate
@@ -77,5 +78,20 @@ def append_shuffle(train_data,train_label,val_data,val_label,test_data,test_labe
 	test_sh_label = test_appl[permutation,:]
 
 	return train_sh,train_sh_label,val_sh,val_sh_label,test_sh,test_sh_label
+
+def plot_imagesNlabels(data, labels)
+	rand_mine = np.random.randint(0,data.shape[0],12)
+	sampled_x = data[rand_mine]
+	sampled_y = labels[rand_mine].reshape(12,10)
+	num_rows = 2
+	num_cols = 6
+	f, ax = plt.subplots(num_rows, num_cols, figsize = (12,5), gridspec_kw = {'wspace':0.03 , 'hspace':0.01}, squeeze = True)
+	for i in range (num_rows):
+		for j in range (num_cols):
+			image_index = i*6 + j
+			ax[i,j].axis("off")
+			ax[i,j].imshow(np.squeeze(sampled_x[image_index]), cmap='gray')
+			ax[i,j].set_title('No. %d' % np.where(sampled_y[image_index] == 1))
+	plt.show()
         
     
